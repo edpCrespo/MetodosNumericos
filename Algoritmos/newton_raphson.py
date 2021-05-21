fc7d09fdc2ad8fd8f424b1fb5c348ca5f79f2f68
edpCrespo <pilii.cres@gmail.com>
2021-05-21 19:38:42 -0300

    Update newton_raphson.py

--- Algoritmos/newton_raphson.py
+++ Algoritmos/newton_raphson.py
@@ -1,12 +1,6 @@
-from sympy import diff
 import numpy as np
-from sympy.abc import x
 
-
-def newton_raphson(x_0, tol, f):
-    # calculo la derivada de f respecto de x
-    f_prime = diff(f, x)
-
+def newton_raphson(x_0, tol, f, f_prime):
     # hago la primer iter
     x_k = x_0
     x_k1 = x_k - (f(x_k) / f_prime(x_k))
@@ -19,6 +13,22 @@ def newton_raphson(x_0, tol, f):
 
     return x_k1, num_iter
 
+def newton_raphson_v2(x_0, tol, f, f_prime, max_iter):
+    # hago la primer iter
+    x_k = np.zeros(max_iter)
+    x_k[0] = x_0
+    x_k[1] = x_k[0] - (f(x_k[0]) / f_prime(x_k[0]))
+    num_iter = 1
 
-rta = newton_raphson(2, 0.00001, lambda x: x ** 2 - 2)
-print(rta)
+    while np.abs(x_k[num_iter] - x_k[num_iter-1]) > tol and num_iter <= max_iter:  # si la diferencia entre iter sigue siendo mas de lo que quiero (tol), sigo
+        x_k[num_iter+1] = x_k[num_iter] - (f(x_k[num_iter]) / f_prime(x_k[num_iter]))
+        num_iter += 1
+
+    return x_k[:num_iter], num_iter
+
+#rta = newton_raphson(2, 0.00001, lambda x: x ** 2 - 2, lambda x: 2*x)
+#print(rta)
+
+
+#rta = newton_raphson(2, 0.00001, lambda x: x ** 2 - 2, lambda x: 2*x)
+#print(rta)
